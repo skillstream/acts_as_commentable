d75fe2502463df3968a1bb692db813f1c5403ac4
Dave Gudge <dave@gudge.uk>
2022-05-13 17:59:11 +0100

    fix: deprecated warning
    warning: Using the last argument as keyword parameters is deprecated; maybe ** should be added to the call

--- lib/commentable_methods.rb
+++ lib/commentable_methods.rb
@@ -57,9 +57,9 @@ module Juixe
             comment_roles.each do |role|
               define_role_based_inflection(role)
             end
-            has_many :all_comments, {:as => :commentable, :inverse_of => :commentable, :dependent => :destroy, class_name: 'Comment'}.merge(join_options)
+            has_many :all_comments, **{:as => :commentable, :inverse_of => :commentable, :dependent => :destroy, class_name: 'Comment'}.merge(join_options)
           else
-            has_many :comments, {:as => :commentable, :inverse_of => :commentable, :dependent => :destroy}.merge(join_options)
+            has_many :comments, **{:as => :commentable, :inverse_of => :commentable, :dependent => :destroy}.merge(join_options)
           end
 
           comment_types.each do |role|
